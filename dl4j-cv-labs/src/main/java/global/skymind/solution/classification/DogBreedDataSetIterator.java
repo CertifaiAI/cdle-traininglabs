b75a619a7a0717b291424e10e9b49147b8accb88
William Ardianto <william.ardianto@skymind.my>
2019-10-11 15:12:01 +0800

    add dataset iterator class

---
+++ dl4j-cv-labs/src/main/java/global/skymind/solution/classification/DogBreedDataSetIterator.java
@@ -0,0 +1,93 @@
+package global.skymind.solution.classification;
+
+import org.datavec.api.io.filters.BalancedPathFilter;
+import org.datavec.api.io.labels.ParentPathLabelGenerator;
+import org.datavec.api.split.FileSplit;
+import org.datavec.api.split.InputSplit;
+import org.datavec.image.loader.BaseImageLoader;
+import org.datavec.image.recordreader.ImageRecordReader;
+import org.datavec.image.transform.ImageTransform;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
+import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
+import org.slf4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+public class DogBreedDataSetIterator {
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(DogBreedDataSetIterator.class);
+
+    private static final String DATA_DIR = CustomModel.class.getClassLoader().getResource("dog-breed-identification/train").getFile();
+
+    //Images are of format given by allowedExtension
+    private static final String [] allowedExtensions = BaseImageLoader.ALLOWED_FORMATS;
+
+    //Random number generator
+    private static final Random rng  = new Random(123);
+
+    private static final int height = 224;
+    private static final int width = 224;
+    private static final int channels = 3;
+    private static final int numClasses = 5;
+
+    private static ParentPathLabelGenerator labelMaker = new ParentPathLabelGenerator();
+    private static InputSplit trainData,testData;
+    private static int batchSize;
+
+    //scale input to 0 - 1
+    private static DataNormalization scaler = new ImagePreProcessingScaler(0, 1);
+    private static ImageTransform transform;
+
+    private static DataSetIterator makeIterator(InputSplit split, boolean training) throws IOException {
+        ImageRecordReader recordReader = new ImageRecordReader(height,width,channels,labelMaker);
+        if (training && transform != null){
+            recordReader.initialize(split,transform);
+        }else{
+            recordReader.initialize(split);
+        }
+        DataSetIterator iter = new RecordReaderDataSetIterator(recordReader, batchSize, 1, numClasses);
+        iter.setPreProcessor(scaler);
+
+        return iter;
+    }
+
+    public static DataSetIterator trainIterator() throws IOException {
+        return makeIterator(trainData, true);
+    }
+
+    public static DataSetIterator testIterator() throws IOException {
+        return makeIterator(testData, false);
+    }
+
+    public static void setup(int batchSizeArg, int trainPerc, ImageTransform imageTransform) throws IOException {
+        transform=imageTransform;
+        setup(batchSizeArg,trainPerc);
+    }
+
+    public static void setup(int batchSizeArg, int trainPerc) throws IOException {
+        batchSize = batchSizeArg;
+
+        File parentDir = new File(DATA_DIR);
+
+        //Files in directories under the parent dir that have "allowed extensions" split needs a random number generator for reproducibility when splitting the files into train and test
+        FileSplit filesInDir = new FileSplit(parentDir, allowedExtensions, rng);
+
+        //The balanced path filter gives you fine tune control of the min/max cases to load for each class
+        BalancedPathFilter pathFilter = new BalancedPathFilter(rng, allowedExtensions, labelMaker);
+        if (trainPerc >= 100) {
+            throw new IllegalArgumentException("Percentage of data set aside for training has to be less than 100%. Test percentage = 100 - training percentage, has to be greater than 0");
+        }
+
+        //Split the image files into train and test
+        InputSplit[] filesInDirSplit = filesInDir.sample(pathFilter, trainPerc, 100-trainPerc);
+        trainData = filesInDirSplit[0];
+        testData = filesInDirSplit[1];
+    }
+
+
+
+
+}
