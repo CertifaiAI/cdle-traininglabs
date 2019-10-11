3b8c67b61f9164a17c855a13ee180c0e97b75a57
William Ardianto <william.ardianto@skymind.my>
2019-10-11 15:12:17 +0800

    add solution custom model

---
+++ dl4j-cv-labs/src/main/java/global/skymind/solution/classification/CustomModel.java
@@ -0,0 +1,186 @@
+package global.skymind.solution.classification;
+
+import org.datavec.api.io.filters.BalancedPathFilter;
+import org.datavec.api.io.labels.ParentPathLabelGenerator;
+import org.datavec.api.split.FileSplit;
+import org.datavec.api.split.InputSplit;
+import org.datavec.image.loader.BaseImageLoader;
+import org.datavec.image.recordreader.ImageRecordReader;
+import org.datavec.image.transform.*;
+import org.deeplearning4j.api.storage.StatsStorage;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+
+import org.deeplearning4j.nn.conf.ConvolutionMode;
+import org.deeplearning4j.nn.conf.GradientNormalization;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.inputs.InputType;
+import org.deeplearning4j.nn.conf.layers.*;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.api.InvocationType;
+import org.deeplearning4j.optimize.listeners.EvaluativeListener;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.ui.api.UIServer;
+import org.deeplearning4j.ui.stats.StatsListener;
+import org.deeplearning4j.ui.storage.FileStatsStorage;
+import org.deeplearning4j.zoo.ZooModel;
+import org.deeplearning4j.zoo.model.VGG16;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
+import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
+import org.nd4j.linalg.learning.config.Adam;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+import org.nd4j.linalg.primitives.Pair;
+import org.deeplearning4j.zoo.*;
+import org.slf4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class CustomModel {
+
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(CustomModel.class);
+
+    private static int epochs = 120;
+    private static int batchSize = 32;
+    private static int seed = 123;
+    private static int numClasses =5;
+
+    private static int height = 224;
+    private static int width = 224;
+    private static int channel = 3;
+
+    private static final Random randNumGen = new Random(seed);
+
+    public static void main(String args[]) throws Exception{
+
+        // image augmentation
+        ImageTransform horizontalFlip = new FlipImageTransform(1);
+        ImageTransform cropImage = new CropImageTransform(25);
+        ImageTransform rotateImage = new RotateImageTransform(randNumGen, 15);
+        ImageTransform showImage = new ShowImageTransform("Image",1000);
+        boolean shuffle = false;
+        List<Pair<ImageTransform,Double>> pipeline = Arrays.asList(
+                new Pair<>(horizontalFlip,0.5),
+                new Pair<>(rotateImage, 0.5),
+                new Pair<>(cropImage,0.3)
+//                ,new Pair<>(showImage,1.0) //uncomment this to show transform image
+        );
+
+        ImageTransform transform = new PipelineImageTransform(pipeline,shuffle);
+
+        DogBreedDataSetIterator.setup(batchSize, 80, transform);
+
+        //create iterators
+        DataSetIterator trainIter = DogBreedDataSetIterator.trainIterator();
+        DataSetIterator testIter = DogBreedDataSetIterator.testIterator();
+
+        //model configuration
+        double nonZeroBias = 0.1;
+        double dropOut = 0.5;
+
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(seed)
+                .weightInit(WeightInit.XAVIER)
+                .activation(Activation.RELU)
+                .updater(new Adam(0.001))
+                .convolutionMode(ConvolutionMode.Same)
+                .gradientNormalization(GradientNormalization.RenormalizeL2PerLayer) // normalize to prevent vanishing or exploding gradients
+                .l2(5 * 1e-4)
+                .list()
+                    .layer(0, new ConvolutionLayer.Builder(new int[]{11,11}, new int[]{4, 4})
+                            .name("cnn1")
+                            .convolutionMode(ConvolutionMode.Truncate)
+                            .nIn(channel)
+                            .nOut(96)
+                            .build())
+                    .layer(1, new LocalResponseNormalization.Builder().build())
+                    .layer(2, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+                            .kernelSize(3,3)
+                            .stride(2,2)
+                            .padding(1,1)
+                            .name("maxpool1")
+                            .build())
+                    .layer(3, new ConvolutionLayer.Builder(new int[]{5,5}, new int[]{1,1}, new int[]{2,2})
+                            .name("cnn2")
+                            .convolutionMode(ConvolutionMode.Truncate)
+                            .nOut(256)
+                            .biasInit(nonZeroBias)
+                            .build())
+                    .layer(4, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, new int[]{3, 3}, new int[]{2, 2})
+                            .convolutionMode(ConvolutionMode.Truncate)
+                            .name("maxpool2")
+                            .build())
+                    .layer(5, new LocalResponseNormalization.Builder().build())
+                    .layer(6, new ConvolutionLayer.Builder()
+                            .kernelSize(3,3)
+                            .stride(1,1)
+                            .convolutionMode(ConvolutionMode.Same)
+                            .name("cnn3")
+                            .nOut(384)
+                            .build())
+                    .layer(7, new ConvolutionLayer.Builder(new int[]{3,3}, new int[]{1,1})
+                            .name("cnn4")
+                            .nOut(384)
+                            .dropOut(0.2)
+                            .biasInit(nonZeroBias)
+                            .build())
+                    .layer(8, new ConvolutionLayer.Builder(new int[]{3,3}, new int[]{1,1})
+                            .name("cnn5")
+                            .nOut(256)
+                            .dropOut(0.2)
+                            .biasInit(nonZeroBias)
+                            .build())
+                    .layer(9, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, new int[]{3,3}, new int[]{2,2})
+                            .name("maxpool3")
+                            .convolutionMode(ConvolutionMode.Truncate)
+                            .build())
+                    .layer(10, new DenseLayer.Builder()
+                            .name("ffn1")
+                            .nOut(4096)
+                            .weightInit(WeightInit.XAVIER)
+                            .biasInit(nonZeroBias)
+                            .dropOut(dropOut)
+                            .build())
+                    .layer(11, new DenseLayer.Builder()
+                            .name("ffn2")
+                            .nOut(4096)
+                            .weightInit(WeightInit.XAVIER)
+                            .biasInit(nonZeroBias)
+                            .dropOut(dropOut)
+                            .build())
+                    .layer(12, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                            .name("output")
+                            .nOut(numClasses)
+                            .activation(Activation.SOFTMAX)
+                .weightInit(WeightInit.XAVIER)
+                .build())
+                    .setInputType(InputType.convolutional(height, width, channel))
+                .build();
+
+
+        //train model and eval model
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+
+        log.info(model.summary());
+
+        UIServer uiServer = UIServer.getInstance();
+        StatsStorage statsStorage = new FileStatsStorage(new File(System.getProperty("java.io.tmpdir"), "ui-stats.dl4j"));
+        uiServer.attach(statsStorage);
+        model.setListeners(
+                new StatsListener( statsStorage),
+                new ScoreIterationListener(5),
+                new EvaluativeListener(trainIter, 1, InvocationType.EPOCH_END),
+                new EvaluativeListener(testIter, 1, InvocationType.EPOCH_END)
+        );
+
+        model.fit(trainIter, epochs);
+    }
+}
