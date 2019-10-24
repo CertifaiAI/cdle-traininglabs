effda9a0db88ccbf5e4573139d4dd8910923e3eb
William Ardianto <william.ardianto@skymind.my>
2019-10-24 17:23:27 +0800

    add ex 1-4

--- dl4j-labs/src/main/java/global/skymind/solution/nd4j/Ex1_CreatingINDArrays.java
+++ dl4j-labs/src/main/java/global/skymind/solution/nd4j/Ex1_CreatingINDArrays.java
@@ -1,4 +1,107 @@
 package global.skymind.solution.nd4j;
 
+import org.nd4j.linalg.api.buffer.DataType;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.exception.ND4JArraySizeException;
+import org.nd4j.linalg.factory.Nd4j;
+
+import java.util.Arrays;
+
 public class Ex1_CreatingINDArrays {
+
+    public static final String BLACK_BOLD = "\033[1;30m";  // BLACK
+    public static final String RED_BOLD = "\033[1;31m";    // RED
+    public static final String GREEN_BOLD = "\033[1;32m";  // GREEN
+    public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
+    public static final String BLUE_BOLD = "\033[1;34m";   // BLUE
+    public static final String PURPLE_BOLD = "\033[1;35m"; // PURPLE
+    public static final String CYAN_BOLD = "\033[1;36m";   // CYAN
+    public static final String WHITE_BOLD = "\033[1;37m";  // WHITE
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    public static void main(String[] args) {
+
+        Nd4j.setDefaultDataTypes(DataType.DOUBLE, DataType.DOUBLE);
+
+        //Initialize INDArrays with zero, one, and scalar value
+        int nRows = 3;
+        int nColumns = 5;
+        INDArray allZeros = Nd4j.zeros(nRows, nColumns);
+        System.out.println(BLUE_BOLD+"\nNd4j.zeros(nRows, nColumns)"+ANSI_RESET);
+        System.out.println(allZeros);
+
+        INDArray allOnes = Nd4j.ones(nRows, nColumns);
+        System.out.println(BLUE_BOLD+"\nNd4j.ones(nRows, nColumns)"+ANSI_RESET);
+        System.out.println(allOnes);
+
+        INDArray allTens = Nd4j.valueArrayOf(nRows, nColumns, 10.0);
+        System.out.println(BLUE_BOLD+"\nNd4j.valueArrayOf(nRows, nColumns, 10.0)"+ANSI_RESET);
+        System.out.println(allTens);
+
+        //Create INDArrays from double[] and double[][] (or, float/int etc Java arrays)
+        double[] vectorDouble = new double[]{1,2,3};
+        INDArray rowVector = Nd4j.create(vectorDouble);
+        System.out.println(BLACK_BOLD + "\nCreate row vector: " + ANSI_RESET);
+        System.out.println(BLUE_BOLD + "Nd4j.create(new double[]{1,2,3})" + ANSI_RESET);
+        System.out.println(rowVector);
+
+        INDArray columnVector = Nd4j.create(vectorDouble, new int[]{3,1});  //Manually specify: 3 rows, 1 column
+        System.out.println(BLACK_BOLD + "\nCreate column vector: " + ANSI_RESET);
+        System.out.println(BLUE_BOLD + "Nd4j.create(new double[]{1,2,3}, new int[]{3,1})" + ANSI_RESET);
+        System.out.println(columnVector);
+
+        double[][] matrixDouble = new double[][]{
+                {1, 2, 3},
+                {4, 5, 6}};
+        INDArray matrix = Nd4j.create(matrixDouble);
+        System.out.println(BLACK_BOLD + "\nCreate matrix with double[][]:" + ANSI_RESET);
+        System.out.println(BLUE_BOLD + "Nd4j.create(new double[][]{{1,2,3},{4,5,6}})" + ANSI_RESET);
+        System.out.println(matrix);
+
+        //Create random INDArrays
+        //uniform random number
+        INDArray uniformRand = Nd4j.rand(nRows,nColumns);
+        System.out.println(BLACK_BOLD + "\nCreate uniform random array:" + ANSI_RESET);
+        System.out.println(BLUE_BOLD + "Nd4j.rand(nRows, nColumns)" + ANSI_RESET);
+        System.out.println(uniformRand);
+
+        INDArray uniformRand3D = Nd4j.rand(new int[]{2, nRows, nColumns});
+        System.out.println(BLACK_BOLD + "\nCreate 3 dimensions uniform random array:" + ANSI_RESET);
+        System.out.println(BLUE_BOLD + "Nd4j.rand(new int[]{2, nRows, nColumns})" + ANSI_RESET);
+        System.out.println(uniformRand3D);
+
+        //gaussian random number
+        INDArray gaussianMeanZeroUnitVariance = Nd4j.randn(new int[]{nRows, nColumns});
+        System.out.println(BLACK_BOLD + "\nCreate random numbers with mean zero and standard deviation one:" + ANSI_RESET);
+        System.out.println(BLUE_BOLD + "Nd4j.randn(new int[]{nRows, nColumns})" + ANSI_RESET);
+        System.out.println(gaussianMeanZeroUnitVariance);
+
+        //Create repeatable random array using seed:
+        long seed = 123;
+        int[] shape = new int[]{nRows, nColumns};
+        INDArray rand1 = Nd4j.rand(shape, seed);
+        INDArray rand2 = Nd4j.rand(shape, seed);
+        System.out.println(BLACK_BOLD +"\nUniform random arrays with same fixed seed:" + ANSI_RESET);
+        System.out.println(BLUE_BOLD + "Nd4j.rand(shape, seed)" + ANSI_RESET);
+        System.out.println(BLACK_BOLD + "rand1" + ANSI_RESET);
+        System.out.println(rand1);
+        System.out.println(BLACK_BOLD + "rand2" + ANSI_RESET);
+        System.out.println(rand2);
+
+        //Other miscellaneous methods
+        INDArray identityMatrix = Nd4j.eye(3);
+        System.out.println(BLACK_BOLD +"\nCreate identity matrix:" + ANSI_RESET);
+        System.out.println(BLUE_BOLD + "Nd4j.eye(3)" + ANSI_RESET);
+        System.out.println(identityMatrix);
+
+        INDArray linspace = Nd4j.linspace(1,20,10); //Values 1 to 10, in 10 steps
+        System.out.println(BLACK_BOLD +"\nGenerate a vector with range of values (start from 1 to 20 in 10 steps):" + ANSI_RESET);
+        System.out.println(BLUE_BOLD + "Nd4j.linspace(1,20,10)" + ANSI_RESET);
+        System.out.println(linspace);
+
+        INDArray diagMatrix = Nd4j.diag(Nd4j.create(new double[]{4,5,6}));
+        System.out.println(BLACK_BOLD +"\nCreate square matrix, with vector along the diagonal:" + ANSI_RESET);
+        System.out.println(BLUE_BOLD + "Nd4j.diag(Nd4j.create(new double[]{4,5,6}))" + ANSI_RESET);
+        System.out.println(diagMatrix);
+    }
 }
--- dl4j-labs/src/main/java/global/skymind/solution/nd4j/Ex4_GettingandSettingINDArrays.java
+++ dl4j-labs/src/main/java/global/skymind/solution/nd4j/Ex4_GettingandSettingINDArrays.java
@@ -1,4 +1,94 @@
 package global.skymind.solution.nd4j;
 
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.indexing.INDArrayIndex;
+import org.nd4j.linalg.indexing.NDArrayIndex;
+
 public class Ex4_GettingandSettingINDArrays {
+    public static final String BLACK_BOLD = "\033[1;30m";  // BLACK
+    public static final String RED_BOLD = "\033[1;31m";    // RED
+    public static final String GREEN_BOLD = "\033[1;32m";  // GREEN
+    public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
+    public static final String BLUE_BOLD = "\033[1;34m";   // BLUE
+    public static final String PURPLE_BOLD = "\033[1;35m"; // PURPLE
+    public static final String CYAN_BOLD = "\033[1;36m";   // CYAN
+    public static final String WHITE_BOLD = "\033[1;37m";  // WHITE
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    public static void main(String[] args) {
+        int nRows = 3;
+        int nColumns = 5;
+        INDArray myArray = Nd4j.rand(new int[]{nRows,nColumns}, 123);
+        System.out.println(BLACK_BOLD + "Default array" + ANSI_RESET);
+        System.out.println(myArray);
+
+        //Get scalar value
+        INDArray scalarVal = myArray.getScalar(0,0); //try getDouble, getInt, and getFloat to see the different
+        System.out.println(BLACK_BOLD + "\nGet scalar value" + ANSI_RESET);
+        System.out.println(BLUE_BOLD + "myArray.getScalar(0,0)" + ANSI_RESET);
+        System.out.println(scalarVal);
+
+        //Set scalar value
+        myArray.putScalar(6,0);
+        System.out.println(BLACK_BOLD + "\nSet scalar value by index" + ANSI_RESET);
+        System.out.println(BLUE_BOLD + "myArray.getScalar(6,0)" + ANSI_RESET);
+        System.out.println(myArray);
+
+        myArray.putScalar(new int[]{1,4},10);
+        System.out.println(BLACK_BOLD + "\nSet scalar value based on given location" + ANSI_RESET);
+        System.out.println(BLUE_BOLD + "myArray.putScalar(new int[]{1,4},10)" + ANSI_RESET);
+        System.out.println(myArray);
+
+        //Get row or column
+        INDArray row = myArray.getRow(0);
+        System.out.println(BLACK_BOLD + "\nGet the first row from myArray" + ANSI_RESET);
+        System.out.println(BLUE_BOLD + "myArray.getRow(0)" + ANSI_RESET);
+        System.out.println(row);
+
+        INDArray rows = myArray.getRows(0,2);
+        System.out.println(BLACK_BOLD + "\nGet multiple rows from myArray" + ANSI_RESET);
+        System.out.println(BLUE_BOLD + "myArray.getRows(0,2)" + ANSI_RESET);
+        System.out.println(rows);
+
+        //Set row or column
+        myArray.putRow(2, Nd4j.zeros(5));
+        System.out.println(BLACK_BOLD + "\nSet row number 2 to zero" + ANSI_RESET);
+        System.out.println(BLUE_BOLD + "myArray.putRow(2, Nd4j.zeros(5))" + ANSI_RESET);
+        System.out.println(myArray);
+
+        //Get an arbitrary sub-arrays based on certain indexes
+        INDArray first3Columns = myArray.get(NDArrayIndex.all(), NDArrayIndex.interval(0,3));
+        System.out.println(BLACK_BOLD + "\nGet first 3 columns" + ANSI_RESET);
+        System.out.println(BLUE_BOLD + "myArray.get(NDArrayIndex.all(), NDArrayIndex.interval(0,3))" + ANSI_RESET);
+        System.out.println(first3Columns);
+
+        INDArray secondRow = myArray.get(NDArrayIndex.point(1), NDArrayIndex.all());
+        System.out.println(BLACK_BOLD + "\nGet second row" + ANSI_RESET);
+        System.out.println(BLUE_BOLD + "myArray.get(NDArrayIndex.point(1), NDArrayIndex.all())" + ANSI_RESET);
+        System.out.println(secondRow);
+
+        INDArray oddColumns = myArray.get(NDArrayIndex.all(), NDArrayIndex.interval(0,2,5));
+        System.out.println(BLACK_BOLD + "\nGet all rows and odd column" + ANSI_RESET);
+        System.out.println(BLUE_BOLD + "myArray.get(NDArrayIndex.all(), NDArrayIndex.interval(0,2,5))" + ANSI_RESET);
+        System.out.println(oddColumns);
+
+        //Set an arbitrary sub-arrays based on certain indexes
+        myArray.put(
+                new INDArrayIndex[]{NDArrayIndex.point(0),NDArrayIndex.all()},
+                Nd4j.zeros(1,5)
+        );
+        System.out.println(BLACK_BOLD + "\nSet a first row and all columns to zero" + ANSI_RESET);
+        System.out.println(BLUE_BOLD + "myArray.put(new INDArrayIndex[]{NDArrayIndex.point(0),NDArrayIndex.all()},Nd4j.zeros(1,5));" + ANSI_RESET);
+        System.out.println(myArray);
+
+        //Alternative way
+        myArray.put(
+                myArray.get(NDArrayIndex.point(0), NDArrayIndex.all()),
+                Nd4j.zeros(1,5)
+        );
+        System.out.println(BLACK_BOLD + "\nSet a first row and all columns to zero" + ANSI_RESET);
+        System.out.println(BLUE_BOLD + "myArray.put(myArray.get(NDArrayIndex.point(0), NDArrayIndex.all()),Nd4j.zeros(1,5));" + ANSI_RESET);
+        System.out.println(myArray);
+    }
 }
