2145b06a8e2b80662bd3d34862055a0e6b4cf883
William Ardianto <william.ardianto@skymind.my>
2019-11-05 16:31:41 +0800

    add exercise1

---
+++ dl4j-labs/src/main/java/global/skymind/training/datavec/Exercise1.java
@@ -0,0 +1,133 @@
+package global.skymind.training.datavec;
+
+import org.apache.commons.io.FileUtils;
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.impl.collection.CollectionRecordReader;
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.records.writer.RecordWriter;
+import org.datavec.api.records.writer.impl.csv.CSVRecordWriter;
+import org.datavec.api.split.FileSplit;
+import org.datavec.api.split.partition.NumberOfRecordsPartitioner;
+import org.datavec.api.split.partition.Partitioner;
+import org.datavec.api.transform.TransformProcess;
+import org.datavec.api.transform.condition.ConditionOp;
+import org.datavec.api.transform.condition.column.CategoricalColumnCondition;
+import org.datavec.api.transform.schema.Schema;
+import org.datavec.api.transform.transform.integer.ReplaceInvalidWithIntegerTransform;
+import org.datavec.api.writable.Text;
+import org.datavec.api.writable.Writable;
+import org.datavec.local.transforms.LocalTransformExecutor;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.nd4j.linalg.dataset.SplitTestAndTrain;
+import org.nd4j.linalg.dataset.ViewIterator;
+import org.nd4j.linalg.dataset.api.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
+import org.nd4j.linalg.dataset.api.preprocessor.NormalizerMinMaxScaler;
+import org.nd4j.linalg.io.ClassPathResource;
+
+import java.io.File;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercise1 {
+    private static File inputFile;
+    public static void main(String[] args) throws Exception {
+        inputFile = new ClassPathResource("datavec/titanic/train.csv").getFile();
+
+        /*
+        Exercise 1: Prepare titanic dataset
+        - read csv file
+        - define schema
+        - define transform process
+        - apply transform process
+        - split into training and test dataset
+        - normalize data
+         */
+
+        // read csv file
+        /*
+        YOUR CODE HERE
+        */
+
+        // define schema
+        /*
+        YOUR CODE HERE
+         */
+
+        // cabin has 687 missing values
+        // embarked has 2 missing values
+        // age has 177 missing values
+        // define transform process
+        /*
+        - remove unused column (PassengerId, Name, Tiket, Cabin)
+        - replace Age missing value with 0
+        - replace Embarked missing value with "S"
+        - convert Sex(category) to integer
+        - convert Pclass, Embarked to one hot encoding
+        */
+        /*
+        YOUR CODE HERE
+        */
+
+        //Process the data:
+//        List<List<Writable>> originalData = new ArrayList<>();
+//        while(rr.hasNext()){
+//            originalData.add(rr.next());
+//        }
+
+        //Apply transform process
+        /*
+        YOUR CODE HERE
+        */
+
+        //Create iterator from processedData
+//        RecordReader collectionRecordReader = new CollectionRecordReader(processedData);
+//        DataSetIterator iterator = new RecordReaderDataSetIterator(collectionRecordReader,processedData.size(),0,2);
+//        DataSet allData = iterator.next();
+
+        //Shuffle and split data into training and test dataset
+        /*
+        YOUR CODE HERE
+        */
+
+        //Create iterator for splitted training and test dataset
+//        DataSetIterator trainIterator = new ViewIterator(trainingData, 4);
+//        DataSetIterator testIterator = new ViewIterator(testData, 2);
+
+        //Normalize data to 0 - 1
+        /*
+        YOUR CODE HERE
+        */
+
+//        System.out.println("Sample of training vector: \n"+ trainIterator.next());
+
+//        writeAndPrint(processedData);
+    }
+
+    private static void writeAndPrint(List<List<Writable>> processedData) throws Exception {
+        // write processed data into file
+        RecordWriter rw = new CSVRecordWriter();
+        File outputFile = new File("titanicTransform.csv");
+        if(outputFile.exists()) outputFile.delete();
+        outputFile.createNewFile();
+
+        Partitioner p = new NumberOfRecordsPartitioner();
+        rw.initialize(new FileSplit(outputFile), p);
+        rw.writeBatch(processedData);
+        rw.close();
+
+        //Print before + after:
+        System.out.println("\n\n---- Original Data File ----");
+        String originalFileContents = FileUtils.readFileToString(inputFile, Charset.defaultCharset());
+        System.out.println(originalFileContents);
+
+        System.out.println("\n\n---- Processed Data File ----");
+        String fileContents = FileUtils.readFileToString(outputFile, Charset.defaultCharset());
+        System.out.println(fileContents);
+    }
+
+
+}
---
+++ dl4j-labs/src/main/java/global/skymind/training/datavec/LoadImageAndTransform.java
@@ -0,0 +1,106 @@
+package global.skymind.training.datavec;
+
+import org.datavec.api.io.filters.BalancedPathFilter;
+import org.datavec.api.io.labels.ParentPathLabelGenerator;
+import org.datavec.api.split.FileSplit;
+import org.datavec.api.split.InputSplit;
+import org.datavec.image.loader.BaseImageLoader;
+import org.datavec.image.recordreader.ImageRecordReader;
+import org.datavec.image.transform.*;
+import org.deeplearning4j.arbiter.util.ClassPathResource;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.primitives.Pair;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class LoadImageAndTransform {
+    //Images are of format given by allowedExtension
+    private static final String [] allowedExtensions = BaseImageLoader.ALLOWED_FORMATS;
+
+    private static final long seed = 12345;
+
+    private static final Random randNumGen = new Random(seed);
+
+    private static final int height = 50;
+    private static final int width = 50;
+    private static final int channels = 3;
+
+    public static void main(String[] args) throws Exception {
+        //DIRECTORY STRUCTURE:
+        //Images in the dataset have to be organized in directories by class/label.
+        //In this example there are ten images in three classes
+        //Here is the directory structure
+        //                                    parentDir
+        //                                  /    |     \
+        //                                 /     |      \
+        //                            labelA  labelB   labelC
+        //Set your data up like this so that labels from each label/class live in their own directory
+        //And these label/class directories live together in the parent directory
+
+        // define image folder location
+        File parentDir = new ClassPathResource("datavec/flowers").getFile();
+
+        //Files in directories under the parent dir that have "allowed extensions" split needs a random number generator for reproducibility when splitting the files into train and test
+        FileSplit fileSplit = new FileSplit(parentDir, allowedExtensions, randNumGen);
+
+        //You do not have to manually specify labels. This class (instantiated as below) will
+        //parse the parent dir and use the name of the subdirectories as label/class names
+        ParentPathLabelGenerator labelMaker = new ParentPathLabelGenerator();
+
+        //The balanced path filter gives you fine tune control of the min/max cases to load for each class
+        BalancedPathFilter pathFilter = new BalancedPathFilter(randNumGen, allowedExtensions, labelMaker);
+
+        //Split the image files into train and test. Specify the train test split as 80%,20%
+        InputSplit[] filesInDirSplit = fileSplit.sample(pathFilter, 80, 20);
+        InputSplit trainData = filesInDirSplit[0];
+        InputSplit testData = filesInDirSplit[1];
+
+        //Specifying a new record reader with the height and width you want the images to be resized to.
+        //Note that the images in this example are all of different size
+        //They will all be resized to the height and width specified below
+        ImageRecordReader trainRR = new ImageRecordReader(height,width,channels,labelMaker);
+        ImageRecordReader testRR = new ImageRecordReader(height,width,channels,labelMaker);
+
+        //Define image transformation
+        FlipImageTransform horizontalFlip = new FlipImageTransform(1);
+        ImageTransform cropImage = new CropImageTransform(5);
+        ImageTransform rotateImage = new RotateImageTransform(randNumGen, 15);
+        ImageTransform showImage = new ShowImageTransform("Image",1000);
+        boolean shuffle = false;
+        List<Pair<ImageTransform,Double>> pipeline = Arrays.asList(
+                new Pair<>(horizontalFlip,0.5),
+                new Pair<>(rotateImage, 0.5),
+                new Pair<>(cropImage,0.3),
+                new Pair<>(showImage,1.0)
+        );
+        ImageTransform transform = new PipelineImageTransform(pipeline,shuffle);
+
+        //Initialize the record reader with the train data and the transform chain
+        trainRR.initialize(trainData,transform);
+        testRR.initialize(testData);
+
+        int numLabels = trainRR.numLabels();
+        int batchSize = 3; // Minibatch size. Here: The number of images to fetch for each call to dataIter.next().
+        int labelIndex = 1; // Index of the label Writable (usually an IntWritable), as obtained by recordReader.next()
+        // List<Writable> lw = recordReader.next();
+        // then lw[0] =  NDArray shaped [1,3,50,50] (1, channels, height, width)
+        //      lw[0] =  label as integer.
+
+        DataSetIterator trainIter = new RecordReaderDataSetIterator(trainRR, batchSize, labelIndex, numLabels);
+
+        int batchIndex = 0;
+        while (trainIter.hasNext()) {
+            DataSet ds = trainIter.next();
+
+            batchIndex += 1;
+            System.out.println("\nBatch number: " + String.valueOf(batchIndex));
+            System.out.println("Feature vector shape: " + Arrays.toString(ds.getFeatures().shape()));
+            System.out.println("Label vector shape: " +Arrays.toString(ds.getLabels().shape()));
+        }
+    }
+}
