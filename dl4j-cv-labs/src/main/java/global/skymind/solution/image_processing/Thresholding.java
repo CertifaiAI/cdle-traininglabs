43ea4f640b04a02a63282d489bf5cbcd38b2f2ca
kenghooi-teoh <tkenghooi@gmail.com>
2020-05-14 11:43:10 +0800

    add image thresholding exercise

---
+++ dl4j-cv-labs/src/main/java/global/skymind/solution/image_processing/Thresholding.java
@@ -0,0 +1,52 @@
+package global.skymind.solution.image_processing;/*
+ *
+ *  * ******************************************************************************
+ *  *  * Copyright (c) 2019 Skymind AI Bhd.
+ *  *  * Copyright (c) 2020 CertifAI Sdn. Bhd.
+ *  *  *
+ *  *  * This program and the accompanying materials are made available under the
+ *  *  * terms of the Apache License, Version 2.0 which is available at
+ *  *  * https://www.apache.org/licenses/LICENSE-2.0.
+ *  *  *
+ *  *  * Unless required by applicable law or agreed to in writing, software
+ *  *  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  *  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  *  * License for the specific language governing permissions and limitations
+ *  *  * under the License.
+ *  *  *
+ *  *  * SPDX-License-Identifier: Apache-2.0
+ *  *  *****************************************************************************
+ *
+ *
+ */
+
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.nd4j.linalg.io.ClassPathResource;
+import static org.bytedeco.opencv.global.opencv_imgcodecs.imread;
+import static org.bytedeco.opencv.global.opencv_imgproc.THRESH_BINARY;
+import static org.bytedeco.opencv.global.opencv_imgproc.threshold;
+
+import java.io.IOException;
+
+/*
+* TASKS:
+* -----
+* 1. Load and display sat_map3.jpg from the resources/image_processing folder
+* 2. Apply threshold(=50) to the image
+* 3. Display the thresholded image
+* * Change the threshold value to observe the effects
+*
+* */
+
+public class Thresholding {
+    public static void main(String[] args) throws IOException {
+        // load image
+        Mat src = imread(new ClassPathResource("image_processing/sat_map3.jpg").getFile().getAbsolutePath());
+        Display.display(src, "Original");
+
+        // Apply threshold to the image
+        Mat dest = new Mat();
+        threshold(src, dest, 50, 255, THRESH_BINARY);
+        Display.display(dest, "Thresholding applied");
+    }
+}
