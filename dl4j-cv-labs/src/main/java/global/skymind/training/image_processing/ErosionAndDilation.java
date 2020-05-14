2a456952b41bcb39bb01545dd64a8f27dff9adc3
kenghooi-teoh <tkenghooi@gmail.com>
2020-05-14 11:31:26 +0800

    add erosion & dilation exercise

---
+++ dl4j-cv-labs/src/main/java/global/skymind/training/image_processing/ErosionAndDilation.java
@@ -0,0 +1,46 @@
+package global.skymind.training.image_processing;/*
+ *
+ *  * ******************************************************************************
+ *  *  * Copyright (c) 2019 Skymind AI Bhd.
+ *  *  * Copyright (c) 2020 CertifAI Sdn. Bhd.
+ *  *  *
+ *  *  * This program and the accompanying materials are made available under the
+ *  *  * terms of the Apache License, Version 2.0 which is available at
+ *  *  * https://www.apache.org/licenses/LICENSE-2.0.
+ *  *  *
+ *  *  * Unless required by applicable law or agreed to in writing, software
+ *  *  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  *  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  *  * License for the specific language governing permissions and limitations
+ *  *  * under the License.
+ *  *  *
+ *  *  * SPDX-License-Identifier: Apache-2.0
+ *  *  *****************************************************************************
+ *
+ *
+ */
+
+/*
+ *
+ * TASKS:
+ * -----
+ * 1. Load and display lena.png from the resources/image_processing folder
+ * 2. Apply erosion to the image
+ * 3. Apply dilation to the image
+ * 4. Display the input image, and the two images after applying erosion and dilation separately
+ *
+ * */
+
+import java.io.IOException;
+
+public class ErosionAndDilation {
+    public static void main(String[] args) throws IOException {
+
+        /*
+         *
+         * ENTER YOUR CODE HERE
+         *
+         * */
+
+    }
+}
