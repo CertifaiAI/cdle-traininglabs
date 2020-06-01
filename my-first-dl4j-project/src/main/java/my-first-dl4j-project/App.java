5869d6a8b63c4596ad9ac8c107849224ff8aff40
chiaweilim <chiawei@skymind.global>
2020-06-01 13:51:09 +0800

    Added template dl4j project

---
+++ my-first-dl4j-project/src/main/java/my-first-dl4j-project/App.java
@@ -0,0 +1,13 @@
+package my-first-dl4j-project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ my-first-dl4j-project/src/test/java/my-first-dl4j-project/AppTest.java
@@ -0,0 +1,20 @@
+package my-first-dl4j-project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
