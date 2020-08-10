874161205680ca198103a41bc833b091ff14d3b0
alvin9964 <guansheng.wong@certifai.ai>
2020-08-10 18:26:35 +0800

    Changed the license comment

--- dl4j-cv-labs/src/main/java/ai/certifai/solution/image_processing/LoadImages.java
+++ dl4j-cv-labs/src/main/java/ai/certifai/solution/image_processing/LoadImages.java
@@ -1,24 +1,21 @@
-package ai.certifai.solution.image_processing;/*
+/*
+ * Copyright (c) 2019 Skymind AI Bhd.
+ * Copyright (c) 2020 CertifAI Sdn. Bhd.
  *
- *  * ******************************************************************************
- *  *  * Copyright (c) 2019 Skymind AI Bhd.
- *  *  * Copyright (c) 2020 CertifAI Sdn. Bhd.
- *  *  *
- *  *  * This program and the accompanying materials are made available under the
- *  *  * terms of the Apache License, Version 2.0 which is available at
- *  *  * https://www.apache.org/licenses/LICENSE-2.0.
- *  *  *
- *  *  * Unless required by applicable law or agreed to in writing, software
- *  *  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- *  *  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- *  *  * License for the specific language governing permissions and limitations
- *  *  * under the License.
- *  *  *
- *  *  * SPDX-License-Identifier: Apache-2.0
- *  *  *****************************************************************************
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
  *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  *
+ * SPDX-License-Identifier: Apache-2.0
  */
+
+package ai.certifai.solution.image_processing;
 
 import org.bytedeco.opencv.opencv_core.*;
 import static org.bytedeco.opencv.global.opencv_imgproc.*;
