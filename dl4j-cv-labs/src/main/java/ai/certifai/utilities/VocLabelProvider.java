8428c68edd7d1ded40333d0f701f11ccea9b037f
yccertifai <yinchuang.sum@certifai.ai>
2021-03-04 08:51:50 +0800

    modify to readable

--- dl4j-cv-labs/src/main/java/ai/certifai/utilities/VocLabelProvider.java
+++ dl4j-cv-labs/src/main/java/ai/certifai/utilities/VocLabelProvider.java
@@ -13,93 +13,115 @@ import org.datavec.image.recordreader.objdetect.ImageObject;
 import org.datavec.image.recordreader.objdetect.ImageObjectLabelProvider;
 
 public class VocLabelProvider implements ImageObjectLabelProvider {
+
+    private static final String OBJECT_START_TAG = "<object>";
+    private static final String OBJECT_END_TAG = "</object>";
+    private static final String NAME_TAG = "<name>";
+    private static final String XMIN_TAG = "<xmin>";
+    private static final String YMIN_TAG = "<ymin>";
+    private static final String XMAX_TAG = "<xmax>";
+    private static final String YMAX_TAG = "<ymax>";
+
     private String annotationsDir;
 
-    public VocLabelProvider(@NonNull String baseDirectory) {
+    public VocLabelProvider(@NonNull String baseDirectory){
         this.annotationsDir = FilenameUtils.concat(baseDirectory, "Annotations");
-        if (!(new File(this.annotationsDir)).exists())
-        {
-            throw new IllegalStateException("Annotations directory does not exist. Annotation files should be present at baseDirectory/Annotations/nnnnnn.xml. Expected location: " + this.annotationsDir);
+
+        if(!new File(annotationsDir).exists()){
+            throw new IllegalStateException("Annotations directory does not exist. Annotation files should be " +
+                    "present at baseDirectory/Annotations/nnnnnn.xml. Expected location: " + annotationsDir);
         }
     }
 
+    @Override
     public List<ImageObject> getImageObjectsForPath(String path) {
         int idx = path.lastIndexOf('/');
         idx = Math.max(idx, path.lastIndexOf('\\'));
-        String filename = path.substring(idx + 1, path.lastIndexOf('.'));
-        String xmlPath = FilenameUtils.concat(this.annotationsDir, filename + ".xml");
+
+        String filename = path.substring(idx+1, path.lastIndexOf('.'));   //-4: ".jpg"
+        String xmlPath = FilenameUtils.concat(annotationsDir, filename + ".xml");
         File xmlFile = new File(xmlPath);
-        if (!xmlFile.exists()) {
+        if(!xmlFile.exists()){
             throw new IllegalStateException("Could not find XML file for image " + path + "; expected at " + xmlPath);
-        } else {
-            String xmlContent;
-            try {
-                xmlContent = FileUtils.readFileToString(xmlFile, "UTF-8");
-            } catch (IOException var17) {
-                throw new RuntimeException(var17);
-            }
-
-            String[] lines = xmlContent.split("\n");
-            List<ImageObject> out = new ArrayList();
-
-            for(int i = 0; i < lines.length; ++i) {
-                if (lines[i].contains("<object>")) {
-                    String name = null;
-                    int xmin = -2147483648;
-                    int ymin = -2147483648;
-                    int xmax = -2147483648;
-                    int ymax = -2147483648;
-
-                    while(true) {
-                        while(!lines[i].contains("</object>")) {
-                            if (name == null && lines[i].contains("<name>")) {
-                                int idxStartName = lines[i].indexOf(62) + 1;
-                                int idxEndName = lines[i].lastIndexOf(60);
-                                name = lines[i].substring(idxStartName, idxEndName);
-                                ++i;
-                            } else if (xmin == -2147483648 && lines[i].contains("<xmin>")) {
-                                xmin = this.extractAndParse(lines[i]);
-                                ++i;
-                            } else if (ymin == -2147483648 && lines[i].contains("<ymin>")) {
-                                ymin = this.extractAndParse(lines[i]);
-                                ++i;
-                            } else if (xmax == -2147483648 && lines[i].contains("<xmax>")) {
-                                xmax = this.extractAndParse(lines[i]);
-                                ++i;
-                            } else if (ymax == -2147483648 && lines[i].contains("<ymax>")) {
-                                ymax = this.extractAndParse(lines[i]);
-                                ++i;
-                            } else {
-                                ++i;
-                            }
-                        }
-
-                        if (name == null) {
-                            throw new IllegalStateException("Invalid object format: no name tag found for object in file " + xmlPath);
-                        }
-
-                        if (xmin == -2147483648 || ymin == -2147483648 || xmax == -2147483648 || ymax == -2147483648) {
-                            throw new IllegalStateException("Invalid object format: did not find all of xmin/ymin/xmax/ymax tags in " + xmlPath);
-                        }
-
-                        out.add(new ImageObject(xmin, ymin, xmax, ymax, name));
-                        break;
-                    }
-                }
-            }
-
-            return out;
         }
+
+        String xmlContent;
+        try{
+            xmlContent = FileUtils.readFileToString(xmlFile, "UTF-8");
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
+        //Normally we'd use Jackson to parse XML, but Jackson has real trouble with multiple XML elements with
+        //  the same name. However, the structure is simple and we can parse it manually (even though it's not
+        // the most elegant thing to do :)
+        String[] lines = xmlContent.split("\n");
+
+        List<ImageObject> out = new ArrayList<>();
+        for( int i=0; i<lines.length; i++ ){
+            if(!lines[i].contains(OBJECT_START_TAG)){
+                continue;
+            }
+            String name = null;
+            int xmin = Integer.MIN_VALUE;
+            int ymin = Integer.MIN_VALUE;
+            int xmax = Integer.MIN_VALUE;
+            int ymax = Integer.MIN_VALUE;
+            while(!lines[i].contains(OBJECT_END_TAG)){
+                if(name == null && lines[i].contains(NAME_TAG)){
+                    int idxStartName = lines[i].indexOf('>') + 1;
+                    int idxEndName = lines[i].lastIndexOf('<');
+                    name = lines[i].substring(idxStartName, idxEndName);
+                    i++;
+                    continue;
+                }
+                if(xmin == Integer.MIN_VALUE && lines[i].contains(XMIN_TAG)){
+                    xmin = extractAndParse(lines[i]);
+                    i++;
+                    continue;
+                }
+                if(ymin == Integer.MIN_VALUE && lines[i].contains(YMIN_TAG)){
+                    ymin = extractAndParse(lines[i]);
+                    i++;
+                    continue;
+                }
+                if(xmax == Integer.MIN_VALUE && lines[i].contains(XMAX_TAG)){
+                    xmax = extractAndParse(lines[i]);
+                    i++;
+                    continue;
+                }
+                if(ymax == Integer.MIN_VALUE && lines[i].contains(YMAX_TAG)){
+                    ymax = extractAndParse(lines[i]);
+                    i++;
+                    continue;
+                }
+
+                i++;
+            }
+
+            if(name == null){
+                throw new IllegalStateException("Invalid object format: no name tag found for object in file " + xmlPath);
+            }
+            if(xmin == Integer.MIN_VALUE || ymin == Integer.MIN_VALUE || xmax == Integer.MIN_VALUE || ymax == Integer.MIN_VALUE){
+                throw new IllegalStateException("Invalid object format: did not find all of xmin/ymin/xmax/ymax tags in " + xmlPath);
+            }
+
+            out.add(new ImageObject(xmin, ymin, xmax, ymax, name));
+        }
+
+        return out;
     }
 
-    private int extractAndParse(String line) {
+    private int extractAndParse(String line){
         int idxStartName = line.indexOf('>') + 1;
         int idxEndName = line.lastIndexOf('<');
         String substring = line.substring(idxStartName, idxEndName);
         return Integer.parseInt(substring);
     }
 
+    @Override
     public List<ImageObject> getImageObjectsForPath(URI uri) {
-        return this.getImageObjectsForPath(uri.toString());
+        return getImageObjectsForPath(uri.toString());
     }
+
 }
