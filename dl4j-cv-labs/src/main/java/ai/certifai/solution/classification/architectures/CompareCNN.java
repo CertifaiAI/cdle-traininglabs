cc83bafda03138cdf8ed3b0982058d6bd26f5e22
kenghooi-teoh <tkenghooi@gmail.com>
2021-03-22 20:53:34 +0800

    refactor the compare CNN exercise

---
+++ dl4j-cv-labs/src/main/java/ai/certifai/solution/classification/architectures/CompareCNN.java
@@ -0,0 +1,83 @@
+package ai.certifai.solution.classification.architectures;/*
+ *
+ *  * ******************************************************************************
+ *  *  * Copyright (c) 2019 Skymind AI Bhd.
+ *  *  * Copyright (c) 2020 CertifAI Sdn. Bhd.
+ *  *  *
+ *  *  * This program and the accompanying materials are made available under the
+ *  *  * terms of the Apache License, Version 2.0 which is available at
+ *  *  * https://www.apache.org/licenses/LICENSE-2.0.
+ *  *  *
+ *  *  * Unless required by applicable law or agreed to in writing, software
+ *  *  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  *  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  *  * License for the specific language governing permissions and limitations
+ *  *  * under the License.
+ *  *  *
+ *  *  * SPDX-License-Identifier: Apache-2.0
+ *  *  *****************************************************************************
+ *
+ *
+ */
+
+import ai.certifai.training.classification.WeatherDataSetIterator;
+import javafx.util.Pair;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.nn.transferlearning.FineTuneConfiguration;
+import org.nd4j.evaluation.classification.Evaluation;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.learning.config.Nesterovs;
+import org.slf4j.Logger;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+
+import static ai.certifai.solution.classification.architectures.CreateCNN.*;
+
+public class CompareCNN {
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(CompareCNN.class);
+
+    private static final int trainPerc = 80;
+    private static final int batchSize = 16;
+
+    public static void main(String[] args) throws IOException, IllegalAccessException {
+
+        // Prepare data.
+        WeatherDataSetIterator.setup(batchSize, trainPerc);
+        DataSetIterator trainIter = WeatherDataSetIterator.trainIterator();
+        DataSetIterator testIter = WeatherDataSetIterator.testIterator();
+
+        // Create configuration for model training.
+        FineTuneConfiguration fineTuneConf = new FineTuneConfiguration.Builder()
+                            .updater(new Nesterovs(5e-5))
+                            .seed(123)
+                            .build();
+
+        // Build 3 models based on different CNN architectures, keep them in an iterator containing pairs of name:computationgraph.
+        Iterator<Pair<String, ComputationGraph>> models = Arrays.asList(
+                        new Pair<>("VGG16", buildVGG16(fineTuneConf)),
+                        new Pair<>("VGG19", buildVGG19(fineTuneConf)),
+                        new Pair<>("SqueezeNet", buildSqueezeNet(fineTuneConf))
+        ).iterator();
+
+
+        // Run experiment: train the 3 models built and compare their evaluation scores.
+        while (models.hasNext()) {
+            Pair<String, ComputationGraph> model = models.next();
+            ComputationGraph graph = model.getValue();
+            String name = model.getKey();
+            System.out.println( name + " MODEL TRAINING STARTS" + "\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" );
+            System.out.println(graph.summary());
+            graph.fit(trainIter, 1);
+            Evaluation eval = graph.evaluate(testIter);
+            System.out.println(name + "\n" + eval.stats());
+            log.info(name + "\n" + eval.stats());
+            System.out.println( name + " MODEL TRAINING ENDS" + "\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n\n\n" );
+            trainIter.reset();
+            testIter.reset();
+        }
+
+
+    }
+}
---
+++ dl4j-cv-labs/src/main/java/ai/certifai/solution/classification/architectures/architectures.README.md
@@ -0,0 +1,6 @@
+In this exercise, we will do a simple experiment to find out the difference in performance among different CNN architectures when trained on a same image dataset.
+
+Tasks:
+1. Train at least 3 CNN models (basing them on different architectures e.g. VGG, ResNet, SqueezeNet etc.) on the weather image dataset. To make interesting comparison, include more variety of CNN designs such as models with/without skip connections, small/high amount of layers etc.
+2. Keep a log of your experiment e.g. the model configurations and their respectively evaluation scores.
+3. State your observations and try to explain the factors contributing to the difference in performance achieved by the models.
