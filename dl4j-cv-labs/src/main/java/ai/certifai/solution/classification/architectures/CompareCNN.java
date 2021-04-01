bc2f0f1ba6404d1dc229b050c4516ad8526e68a4
Kian Yang Lee <user.name>
2021-04-01 09:55:14 +0800

    Include listeners

--- dl4j-cv-labs/src/main/java/ai/certifai/solution/classification/architectures/CompareCNN.java
+++ dl4j-cv-labs/src/main/java/ai/certifai/solution/classification/architectures/CompareCNN.java
@@ -22,13 +22,21 @@ package ai.certifai.solution.classification.architectures;/*
 
 import ai.certifai.training.classification.WeatherDataSetIterator;
 import javafx.util.Pair;
+import org.deeplearning4j.core.storage.StatsStorage;
 import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.nn.transferlearning.FineTuneConfiguration;
+import org.deeplearning4j.optimize.api.InvocationType;
+import org.deeplearning4j.optimize.listeners.EvaluativeListener;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.ui.api.UIServer;
+import org.deeplearning4j.ui.model.stats.StatsListener;
+import org.deeplearning4j.ui.model.storage.InMemoryStatsStorage;
 import org.nd4j.evaluation.classification.Evaluation;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.learning.config.Nesterovs;
 import org.slf4j.Logger;
 
+import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -40,13 +48,15 @@ public class CompareCNN {
 
     private static final int trainPerc = 80;
     private static final int batchSize = 16;
+    private static DataSetIterator trainIter;
+    private static DataSetIterator testIter;
 
     public static void main(String[] args) throws IOException, IllegalAccessException {
 
         // Prepare data.
         WeatherDataSetIterator.setup(batchSize, trainPerc);
-        DataSetIterator trainIter = WeatherDataSetIterator.trainIterator();
-        DataSetIterator testIter = WeatherDataSetIterator.testIterator();
+        trainIter = WeatherDataSetIterator.trainIterator();
+        testIter = WeatherDataSetIterator.testIterator();
 
         // Create configuration for model training.
         FineTuneConfiguration fineTuneConf = new FineTuneConfiguration.Builder()
@@ -69,6 +79,9 @@ public class CompareCNN {
             String name = model.getKey();
             System.out.println( name + " MODEL TRAINING STARTS" + "\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" );
             System.out.println(graph.summary());
+
+            // Visualize network training using dashboard and set up listener to capture information during training.
+            getListeners(graph);
             graph.fit(trainIter, 1);
             Evaluation eval = graph.evaluate(testIter);
             System.out.println(name + "\n" + eval.stats());
@@ -77,7 +90,15 @@ public class CompareCNN {
             trainIter.reset();
             testIter.reset();
         }
-
-
+    }
+    private static void getListeners(ComputationGraph model){git
+        StatsStorage storage = new InMemoryStatsStorage();
+        UIServer server = UIServer.getInstance();
+        server.attach(storage);
+        model.setListeners(
+                new StatsListener(storage),
+                new ScoreIterationListener(1),
+                new EvaluativeListener(trainIter, 1, InvocationType.EPOCH_END),
+                new EvaluativeListener(testIter, 1, InvocationType.EPOCH_END));
     }
 }
