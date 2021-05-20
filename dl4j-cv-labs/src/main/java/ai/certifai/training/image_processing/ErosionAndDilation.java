ab96eb4c3d09fbbe93935c4f199dd4ffb392cfa5
fariskamaludin-skymind <muhammad.faris@skymind.my>
2021-05-20 22:29:22 +0800

    Update license headers for every java & pom files in dl4j-cv-labs

--- dl4j-cv-labs/src/main/java/ai/certifai/training/image_processing/ErosionAndDilation.java
+++ dl4j-cv-labs/src/main/java/ai/certifai/training/image_processing/ErosionAndDilation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 CertifAI Sdn. Bhd.
+ * Copyright (c) 2020-2021 CertifAI Sdn. Bhd.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Apache License, Version 2.0 which is available at
@@ -12,6 +12,7 @@
  * under the License.
  *
  * SPDX-License-Identifier: Apache-2.0
+ *
  */
 
 package ai.certifai.training.image_processing;
